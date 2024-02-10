234a654bd201a2b02fb8764bee30a5ac9336e585
mateuslph <mlptche@gmail.com>
2024-02-10 20:51:01 -0300

    estatisticas ok

--- apiseriesfilmes/src/main/java/com/mlp/apiseriesfilmes/principal/Principal.java
+++ apiseriesfilmes/src/main/java/com/mlp/apiseriesfilmes/principal/Principal.java
@@ -9,10 +9,7 @@ import com.mlp.apiseriesfilmes.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -46,23 +43,35 @@ public class Principal {
 //                .collect(Collectors.toList()); // gera uma lista imutavel
                 .toList();  // pode modificar a lista
 
-        System.out.println("\nTop 10 episódios:");
-        dadosEpisodios.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .peek(e -> System.out.println("Primeiro filtro (N/A) " + e))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-//                .peek(e -> System.out.println("Ordenado" + e))
-                .limit(10)
-                .map(e -> e.titulo().toUpperCase())
-                .forEach(System.out::println);
+//        System.out.println("\nTop 10 episódios:");
+//        dadosEpisodios.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primeiro filtro (N/A) " + e))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+////                .peek(e -> System.out.println("Ordenado" + e))
+//                .limit(10)
+//                .map(e -> e.titulo().toUpperCase())
+//                .forEach(System.out::println);
 
-//        List<Episodio> episodios = temporadas.stream()
-//                .flatMap(t -> t.episodios().stream()
-//                        .map(d -> new Episodio(t.numeroEpisodio(), d))
-//                ).collect(Collectors.toList());
-//
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroEpisodio(), d))
+                ).collect(Collectors.toList());
+
 //        episodios.forEach(System.out::println);
-//
+
+//        System.out.println("Digite um trecho do título do episódio...");
+//        var trechoDoTitulo = sc.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoDoTitulo.toUpperCase()))
+//                .findFirst();
+//        if (episodioBuscado.isPresent()) {
+//            System.out.println("Episódio encontrado:");
+//            System.out.println("Temporada: " + episodioBuscado.get());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
+
 //        System.out.println("A partir de que ano você quer ver os episódios?");
 //        var ano = sc.nextInt();
 //        sc.nextLine();
@@ -75,5 +84,20 @@ public class Principal {
 //                                " Episodio: " + e.getTitulo() +
 //                                " Data de Lançamento: " + e.getDataLancamento().format(formatador)
 //                ));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvliacao)));
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("quantidade: " + est.getCount());
+
     }
 }
